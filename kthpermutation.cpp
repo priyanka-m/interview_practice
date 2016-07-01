50f54b913dc5f08dc1b8dc804806826b068519de
Priyanka Menghani <priyanka.menghani@gmail.com>
2016-06-30 23:32:47 -0700

    changes ko kthpermutation, fixed code.

--- kthpermutation.cpp
+++ kthpermutation.cpp
@@ -19,29 +19,35 @@ void printVector(vector<int>& v) {
 }
 
 void kthPermutation(vector<int> v, int k) {
-	int i = 0, nextBig = i + 1;
-	for (;k > 1;) {
-		if (k < fact(v.size() - i - 1)) {
-			i++;
-			cout << "i = "<<i<<endl;
-			printVector(v);
-		} else if (k > fact(v.size() - i - 1)) {
-			k -= fact(v.size() - i - 1);
-			swap(v[i], v[nextBig]);
-		} else {
-			reverse(v.begin() + i + 1, v.end());
-			k -= fact(v.size() - i - 1);
-		}
-		nextBig += 1;
+
+	vector<int> output;
+
+	int i;
+
+	while (v.size() > 0) {
+		// Find the index from which the resulting permutation will start.
+		// For example, if we want to find the 15th permutation for v = {1,2,3,4}
+		// i = (15 / )
+		i = (k / fact(v.size() - 1));
+		
+		// Find which permutation starting with the ith index is needed
+		k = (k % fact(v.size() - 1));
+
+		output.push_back(v[i]);
+
+		// Remove that element from the remaining array
+		v.erase(v.begin() + i);
+		
 	}
-	printVector(v);
+	
+	printVector(output);
 }
 
 int main() {
 	vector<int> v = {3, 4, 5, 7};
-	// kthPermutation(v, 4);
-	// kthPermutation(v, 8);
-	// kthPermutation(v, 6);
+	kthPermutation(v, 4);
+	kthPermutation(v, 8);
+	kthPermutation(v, 6);
 	kthPermutation(v, 15);
 	return 0;
 }
